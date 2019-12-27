75a877fb02ff2d75c8ee3aeecff043768593d899
Alexis Barbier <barbier-alexis@hotmail.fr>
2019-12-27 19:00:17 +0100

    First commit

---
+++ chat/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <Fragment>
+        <h1>CHAT APP</h1>
+    </Fragment>
+  );
+}
+
+export default Navbar;
